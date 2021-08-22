101991d282e9a1994005b81da19561ea4c7a486f
evandhiggins <evan.higgins06@gmail.com>
2021-08-22 10:26:07 -0500

    Initial commit

---
+++ app/src/main/java/com/higgins/dndnotes/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.higgins.dndnotes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.higgins.dndnotes.ui.theme.DNDNotesTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DNDNotesTheme {
+                Greeting("Foo")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DNDNotesTheme {
+        Greeting("Android")
+    }
+}
