e1037c0b1c5ff2dcf11e3f8a152f0929e132728f
evandhiggins <evan.higgins06@gmail.com>
2021-09-06 21:45:30 -0500

    Temp commit. Still trying to figure out how to make focus stuff work.

--- app/src/main/java/com/higgins/dndjournal/screens/journalentrydetail/JournalEntryDetail.kt
+++ app/src/main/java/com/higgins/dndjournal/screens/journalentrydetail/JournalEntryDetail.kt
@@ -1,9 +1,10 @@
 package com.higgins.dndjournal.screens.journalentrydetail
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
@@ -11,15 +12,13 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.focus.FocusDirection
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.*
 import androidx.compose.ui.platform.LocalFocusManager
@@ -27,16 +26,16 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.higgins.dndjournal.R
-import com.higgins.dndjournal.composables.NewEntryButton
 import com.higgins.dndjournal.db.entrybullet.EntryBullet
 import com.higgins.dndjournal.db.journalentry.JournalEntry
-import com.higgins.dndjournal.main.Route
-import com.higgins.dndjournal.util.applyIf
-import com.higgins.dndjournal.util.onBackspace
+import com.higgins.dndjournal.util.interceptBackspace
+import com.higgins.dndjournal.util.interceptEnter
+import com.higgins.dndjournal.util.interceptKeyPress
 
 @ExperimentalComposeUiApi
 @Composable
@@ -62,13 +61,13 @@ fun JournalEntryDetail(
             listOf()
         )
 
-    val focusRequester = remember { FocusRequester() }
-    val focusedIndex: MutableState<Int?> = remember { mutableStateOf(null) }
     val focusManager = LocalFocusManager.current
     JournalEntryDetailColumn(
         entryBullets,
+        focusManager,
         updateBullet = { entryId, content ->
             journalEntryDetailViewModel.persistBulletContent(entryId, content)
+            focusManager.moveFocus(FocusDirection.Down)
         },
         onCancel = { entryId ->
             journalEntryDetailViewModel.deleteEntry(entryId)
@@ -83,12 +82,19 @@ fun JournalEntryDetail(
 @Composable
 fun JournalEntryDetailColumn(
     entryBullets: List<EntryBullet>,
+    focusManager: FocusManager,
     updateBullet: (Int, String) -> Unit,
     onCancel: (entryId: Int) -> Unit,
     newBullet: () -> Unit,
 ) {
-    LazyColumn {
-        itemsIndexed(entryBullets) { index, entryBullet ->
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxHeight(1f)
+            .clickable {
+                println("CLICK!!!!");
+                //focusManager.clearFocus()
+            }) {
+        items(entryBullets) { entryBullet ->
             JournalEntryBullet(
                 text = entryBullet.content,
                 onFinishUpdate = { content ->
@@ -119,19 +125,10 @@ fun JournalEntryBullet(
     Row {
         val fontSize = 20
         val textStyle = TextStyle(color = Color.Black, fontSize = fontSize.sp)
-        Box(
-            modifier = Modifier.padding((fontSize / 2).dp)
-        ) {
-            BulletPoint()
-        }
+
         val textField = remember { mutableStateOf(TextFieldValue(text)) }
-        val onBackspace = {
-            if (textField.value.text.isEmpty()) {
-                onCancel()
-                false
-            }
-            true
-        }
+
+        BulletPoint(fontSize.dp)
         BasicTextField(
             value = textField.value,
             onValueChange = {
@@ -140,22 +137,19 @@ fun JournalEntryBullet(
             textStyle = textStyle,
             modifier = modifier
                 .fillMaxWidth(1f)
-                .onKeyEvent { keyEvent ->
-                    if (keyEvent.type == KeyEventType.KeyUp && keyEvent.key == Key.Backspace) {
-                        onBackspace()
-                    }
-                    true
+                .focusable()
+                .interceptBackspace(textField.value.text.isEmpty()) {
+                    onCancel()
+                }
+                .interceptEnter {
+                    println("asdfasdfasdfasdf")
                 },
-            keyboardOptions = KeyboardOptions(
-                imeAction = ImeAction.Next
-            ),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Default),
             // TODO: Make it so that pressing enter defocuses the current bullet, and whenever a
             // new bullet is added it is automatically focused.
-            keyboardActions = KeyboardActions(
-                onNext = {
-                    onFinishUpdate(textField.value.text)
-                }
-            )
+            keyboardActions = KeyboardActions(onAny = {
+                onFinishUpdate(textField.value.text)
+            })
         ) {
             it()
         }
@@ -163,12 +157,17 @@ fun JournalEntryBullet(
 }
 
 @Composable
-fun BulletPoint() {
-    Card(
-        shape = CircleShape,
-        modifier = Modifier.size(8.dp, 8.dp),
-        backgroundColor = Color.Black
-    ) {}
+fun BulletPoint(fontSize: Dp) {
+    Box(
+        modifier = Modifier.padding(fontSize / 2)
+    ) {
+        Card(
+            shape = CircleShape,
+            modifier = Modifier.size(8.dp, 8.dp),
+            backgroundColor = Color.Black
+        ) {}
+    }
+
 }
 
 @Composable
--- app/src/main/java/com/higgins/dndjournal/util/ModifierExtensions.kt
+++ app/src/main/java/com/higgins/dndjournal/util/ModifierExtensions.kt
@@ -12,9 +12,31 @@ fun Modifier.applyIf(condition: Boolean, func: (Modifier) -> Unit): Modifier {
     return this
 }
 
+
 @ExperimentalComposeUiApi
-fun Modifier.onBackspace(onBackspace: () -> Boolean): Modifier {
-    this
+fun Modifier.interceptKeyPress(key: Key, condition: Boolean, onPress: () -> Unit): Modifier {
+    this.onKeyEvent { keyEvent ->
+        if (keyEvent.type == KeyEventType.KeyUp && keyEvent.key == key && condition) {
+            onPress()
+            false
+        }
+        true
+    }
 
     return this
+}
+
+@ExperimentalComposeUiApi
+fun Modifier.interceptBackspace(condition: Boolean, onBackspace: () -> Unit): Modifier {
+    return this.interceptKeyPress(Key.Backspace, condition, onBackspace)
+}
+
+@ExperimentalComposeUiApi
+fun Modifier.interceptEnter(onEnter: () -> Unit): Modifier {
+    return this.interceptEnter(true, onEnter)
+}
+
+@ExperimentalComposeUiApi
+fun Modifier.interceptEnter(condition: Boolean, onEnter: () -> Unit): Modifier {
+    return this.interceptKeyPress(Key.Enter, condition, onEnter)
 }
